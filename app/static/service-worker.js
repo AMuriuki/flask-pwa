6a0db70175f803ee46ffc62861643c5010ab181f
arnold <arnoldnderitu@gmail.com>
2020-04-26 23:35:42 +0300

    caching strategies

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,40 +1,41 @@
-const CACHE_NAME = 'static-cache';
+const CACHE_NAME = 'flask-app-cache-v1';
 
 const FILES_TO_CACHE = [
-  '../templates/index.html',
+  '/static/offline.html',
+  '/static/css/base.css',
+  '/index',  
 ];
 
 self.addEventListener('install', (evt) => {
-    console.log('[ServiceWorker] Install');
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
+  console.log('Installing Service Worker');
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Pre-caching pages for offline use');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
 
-  self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-    );
-    self.clients.claim();
-  });
+self.addEventListener('activate', (evt) => {
+  console.log('Activating Service Worker');
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
 
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request);
-      })
-    );
-  }); 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
